952d5c9257f726dbcff4e6b6a208de1c9d022543
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2024-01-26 22:39:33 +0000

    Minor changes

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from fastapi import Form, File, FastAPI
 from fastapi.responses import FileResponse, Response
 from re import sub
 from grading import grade
-import uvicorn
 
 app: FastAPI = FastAPI()
 
 @app.get("/")
 def root():
     return FileResponse("index.html")
+
+@app.get("/template")
+def root():
+    return FileResponse("template.jpg")
 
 @app.post("/grade")
 def submit(hl_correct_answers = Form(...), sl_correct_answers = Form(...), file = File(...)):
