fb1e3fb2bed95a46c2ac8800f3a30844e5babadb
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2024-01-26 17:34:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import Form, File, FastAPI
+from fastapi.responses import FileResponse, Response
+from re import sub
+from grading import grade
+
+app: FastAPI = FastAPI()
+
+@app.get("/")
+def root():
+    return FileResponse("index.html")
+
+@app.post("/grade")
+def submit(hl_correct_answers = Form(...), sl_correct_answers = Form(...), file = File(...)):
+    return Response(content=grade(file.file.read(), [list(sub(r'[^A-D]', '', hl_correct_answers.upper())), list(sub(r'[^A-D]', '', sl_correct_answers.upper()))]), media_type="application/pdf", headers={"Content-Disposition": "inline; filename=output.pdf"})
